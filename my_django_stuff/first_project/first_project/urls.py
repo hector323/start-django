b850aaf54bec154d3ee94e9e8b29721d59b534e2
Hector Salgado <53808824+hector323@users.noreply.github.com>
2020-12-26 17:44:05 -0800

    updated files

--- my_django_stuff/first_project/first_project/urls.py
+++ my_django_stuff/first_project/first_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from first_app import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', views.index,name='index'),
+    url(r'^admin/', admin.site.urls),
 ]
